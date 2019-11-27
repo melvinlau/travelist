7114844a693620ffbf5a73125c2d7c89bbc2facb
Melvin Lau <lau.melvin@icloud.com>
2019-11-27 23:46:15 +0000

    Added static header to display Travelist

--- client/src/App.js
+++ client/src/App.js
@@ -12,24 +12,35 @@ import PackingList from './components/PackingList';
 
 function App() {
   return (
-    <Router>
-      <Link to="/">Step 1: Start view</Link> &nbsp;
-      <Link to="/activities">Step 2: Activities</Link> &nbsp;
-      <Link to="/travelist">Step 3: Travelist</Link> &nbsp;
-      {/* A <Switch> looks through its children <Route>s and
-      renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/activities">
-          <ActivityList />
-        </Route>
-        <Route path="/travelist">
-          <PackingList />
-        </Route>
-        <Route path="/">
-          <StartForm />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Travelist</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <Router>
+            <Link to="/">Step 1: Start view</Link> &nbsp;
+            <Link to="/activities">Step 2: Activities</Link> &nbsp;
+            <Link to="/travelist">Step 3: Travelist</Link> &nbsp;
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/activities">
+                <ActivityList />
+              </Route>
+              <Route path="/travelist">
+                <PackingList />
+              </Route>
+              <Route path="/">
+                <StartForm />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </div>
+    </div>
   );
 }
 
