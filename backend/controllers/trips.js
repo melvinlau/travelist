212ae436b4bd803dc1b7ac381ba31f1685ac86f5
Olly Holovchenko <oleh.holo@gmail.com>
2019-11-27 10:25:47 +0000

    Update routes and controllers. getTripByUserId is still not working.

--- backend/controllers/trips.js
+++ backend/controllers/trips.js
@@ -1,7 +1,7 @@
 const HttpError = require("../models/http-error");
 const Trip = require("../models/trip");
 
-const getTripById = (req, res, next) => {
+const getTripById = async (req, res, next) => {
   const tripId = req.params.tid;
   let trip;
 
@@ -9,46 +9,45 @@ const getTripById = (req, res, next) => {
     trip = await Trip.findById(tripId);
   } catch (err) {
     const error = new HttpError(
-      'Something went wrong, could not find a place.', 500
+      "Something went wrong, could not find a place.",
+      500
     );
     return next(error);
   }
 
-
   if (!trip) {
     const error = new HttpError(
-      "Could not find a trip for the provided id.", 
+      "Could not find a trip for the provided id.",
       404
-      );
+    );
     return next(error);
   }
 
   res.json({ trip: trip.toObject({ getters: true }) });
 };
 
-const getTripsByUserId = (req, res, next) => {
-  const userId = req.params.uid;
+// const getTripsByUserId = async (req, res, next) => {
+//   const userId = req.params.uid;
 
-  let trips;
+//   let trips;
+//   try {
+//     places = await Trip.find({ user: userId });
+//   } catch (err) {
+//     const error = new HttpError(
+//       "Fetching places failed, please try again later",
+//       500
+//     );
+//     return next(error);
+//   }
 
-  try {
-    trips = await Trip.find({ user: userId });
-  } catch (err) {
-    const error = new HttpError(
-      'Fetching places failed, please try again later',
-      500
-    );
-    return next(error);
-  }
+//   if (!trips || trips.length === 0) {
+//     return next(
+//       new HttpError("Could not find trips for the provided user id.", 404)
+//     );
+//   }
 
-  if (!trips || trips.length === 0) {
-    return next(
-      new HttpError("Could not find a trips for the provided user id.", 404)
-    );
-  }
-
-  res.json({ trips: trips.map(trip => trip.toObject({getters: true})) });
-};
+//   res.json({ trips: trips.map(trip => trip.toObject({ getters: true })) });
+// };
 
 const createTrip = async (req, res, next) => {
   const { destination, dateFrom, dateTo, activity, user } = req.body;
@@ -131,7 +130,7 @@ const deleteTrip = async (req, res, next) => {
 };
 
 exports.getTripById = getTripById;
-exports.getTripByUserId = getTripsByUserId;
+// exports.getTripByUserId = getTripsByUserId;
 exports.createTrip = createTrip;
 exports.updateTrip = updateTrip;
 exports.deleteTrip = deleteTrip;
