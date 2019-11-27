10be37610ed62c061dbcd96665c5c3bfeb540d6f
Melvin Lau <lau.melvin@icloud.com>
2019-11-27 22:54:28 +0000

    Configured Enzyme. Wrote and passed a React component test.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { shallow } from 'enzyme';
+import App from './App';
 
 it('Sample passing test', () => {
   expect(true).toEqual(true);
+});
+
+describe('<App />', ()=> {
+
+  it('renders a single <Router /> component', () => {
+    const app = shallow(<App />);
+    expect(app.find(Router).length).toBe(1);
+  });
+
 });
