878a7487d8fea9278c127518cd775a2bcabf6fdc
Melvin Lau <lau.melvin@icloud.com>
2019-11-27 22:00:41 +0000

    Installed and set up react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
 import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
-  return 'Hello world!';
+  return (
+    <Router>
+      <Link to="/">Step 1: Start view</Link> &nbsp;
+      <Link to="/activities">Step 2: Activities</Link> &nbsp;
+      <Link to="/travelist">Step 3: Travelist</Link> &nbsp;
+      {/* A <Switch> looks through its children <Route>s and
+      renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/activities">
+          Activities view
+        </Route>
+        <Route path="/travelist">
+          Travelist view
+        </Route>
+        <Route path="/">
+          Start view
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
