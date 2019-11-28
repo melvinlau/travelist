bc2ede978be22ddc163ea4b2ce4fd7706b86b451
Olly Holovchenko <oleh.holo@gmail.com>
2019-11-28 07:15:03 +0000

    Add Users controller methods. Routes tested with Postman

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -7,7 +7,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
-  trips: { type: String, required: true },
+  trips: { type: Array },
 });
 
 userSchema.plugin(uniqueValidator);
