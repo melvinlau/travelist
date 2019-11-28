98f1aa84e9dd1b0ed3c5e910222d2d2318eb85cb
Brad Phillips <bjrp@protonmail.com>
2019-11-28 16:50:39 +0000

    API call testing

--- client/src/components/StartForm.js
+++ client/src/components/StartForm.js
@@ -28,7 +28,13 @@ function StartForm({ setDestination, setDateTo, setDateFrom }) {
     setDestination(userInputDestination);    
     setDateTo(dateTo);
     setDateFrom(dateFrom);
+    postStartFormToAPI()
   }
+  //test output 
+  const postStartFormToAPI = () => {
+    console.log(dateTo)
+  }
+
 
   return (
     <div className="start-form">
