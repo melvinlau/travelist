7d741c1816425ca906c04506d6e008e9ddc3c320
Melvin Lau <lau.melvin@icloud.com>
2019-11-29 15:30:01 +0000

    Tested POST request to create a new trip

--- client/cypress/integration/CreateTrip.spec.js
+++ client/cypress/integration/CreateTrip.spec.js
@@ -1,17 +1,29 @@
 /// <reference types="Cypress" />
 
-// import { StartForm } from '../../src/components/StartForm'; // Double check this
+import axios from 'axios';
 import { submitDestinationAndDate } from './_helpers';
 
 context('User visits start page, fills form correctly and clicks button', () => {
 
-  beforeEach(() => {
-    // Create a spy
-    submitDestinationAndDate();
-  });
-
   it('generates a POST request and creates a trip', () => {
-    // TODO
+
+    cy.server();
+    cy.route('POST', 'http://localhost:3001/api/trips/').as('createTripRequest');
+    submitDestinationAndDate();
+    cy.wait('@createTripRequest');
+    cy.get('@createTripRequest').then(xhr => {
+      expect(xhr.method).to.eq('POST');
+      expect(xhr.status).to.eq(201);
+      expect(xhr.requestBody).to.contain(
+        {
+          "destination": "London",
+          "dateFrom": "2019-12-06",
+          "dateTo": "2019-12-08"
+        }
+      );
+      expect(xhr.responseBody.trip).to.have.property('_id');
+    });
+
   })
 
 });
