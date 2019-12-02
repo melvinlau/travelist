04cc6efff904af9cafe184129f2376b7e286e57d
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-02 21:50:20 +0000

    Remove getUsers from User controller

--- backend/src/controllers/usersControllers.js
+++ backend/src/controllers/usersControllers.js
@@ -5,20 +5,6 @@ const jwt = require('jsonwebtoken');
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
-const getUsers = async (req, res, next) => {
-  let users;
-  try {
-    users = await User.find({}, '-password');
-  } catch (err) {
-    const error = new HttpError(
-      'Fetching users failed, please try again later.',
-      500,
-    );
-    return next(error);
-  }
-  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
-};
-
 const signup = async (req, res, next) => {
   // eslint-disable-next-line object-curly-newline
   const { name, email, password } = req.body;
@@ -155,6 +141,5 @@ const login = async (req, res, next) => {
   });
 };
 
-exports.getUsers = getUsers;
 exports.signup = signup;
 exports.login = login;
