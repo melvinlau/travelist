c5e61e7ea051a6d79ff5aba2a0e15b0fc900fac8
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-03 23:03:51 +0000

    Finish TripList & Navbar views

--- client/src/components/test/TripsListNew.js
+++ client/src/components/test/TripsListNew.js
@@ -8,75 +8,86 @@ function TripsListNew() {
 
   return (
     <div>
-      <div className="container-fluid">
-        <nav className="navbar navbar-expand-lg navbar-light bg-white">
-          <div className="row justify-content-center">
-            <div className="col-8">
-              <a className="navbar-brand" href="#">
-                Travelist
-              </a>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
+      <nav className="navbar navbar-expand-lg navbar-light bg-white static-top mb-4 shadow">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <h4>
+              <strong>Travelist</strong>
+            </h4>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <ul className="navbar-nav ml-auto">
-                  <li className="nav-item active">
-                    <a className="nav-link" href="#">
-                      New Trip
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      My Trip
-                    </a>
-                  </li>
-                </ul>
-                <button
-                  className="btn btn-outline-success my-2 my-sm-0"
-                  type="submit"
-                >
-                  Login
-                </button>
-              </div>
-            </div>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  New Trip
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  My Trip
+                </a>
+              </li>
+            </ul>
+            <button className="btn btn-outline-secondary my-2 my-sm-0">
+              Login
+            </button>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
+
       <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-6">
+        <div className="row">
+          <div className="col-12 d-flex justify-content-center flex-column align-items-center pb-4">
+            <img src="./images/char.png" style={{ width: "100px" }} />
+            <h2>
+              <strong>Where are you going next?</strong>
+            </h2>
+            <button className="btn btn-primary btn-lg">NEW TRIP</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 d-flex justify-content-center">
             <div className="card" style={{ width: "32rem" }}>
-              <img
-                src={
-                  "https://i.pinimg.com/564x/2e/f2/e4/2ef2e4499e030b2c7e4709d6309e0a5f.jpg"
-                }
+              <div
                 className="card-img-top"
-              />
-              <div className="card-body">
-                <h4 className="card-title">
-                  <strong>Destination</strong>
-                </h4>
+                style={{
+                  background:
+                    "url(https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80) no-repeat center center",
+                  backgroundSize: "100%",
+                  height: "16rem"
+                }}
+              ></div>
+              <div className="card-body pt-4">
+                <h3 className="card-title">
+                  <strong>Rio de Janeiro</strong>
+                </h3>
                 <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  <span className="text-muted small">FROM:</span>{" "}
+                  16/12/2019&nbsp;&nbsp;&nbsp;
+                  <span className="text-muted small">TO:</span> 23/01/2020
                 </p>
-                <a href="#" className="btn btn-primary">
+                <a href="#" className="btn btn-warning">
                   Go somewhere
                 </a>
               </div>
             </div>
+          </div>
+        </div>
+        <div className="raw justify-content-center">
+          <div className="col text-muted text-center small pt-5 pb-3">
+            Made with ❤️ by Melvin, Zaira, Olly &amp; Brad
           </div>
         </div>
       </div>
