f1189fbb6954586112a3bbcaab1a20ce43c71c28
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-04 07:11:29 +0000

    Finish new PackingList view component

--- client/src/components/new/PackingNew.js
+++ client/src/components/new/PackingNew.js
@@ -22,7 +22,7 @@ function TripsListNew() {
         </div>
         <div className="row">
           <div className="col-12 d-flex justify-content-center flex-column align-items-center">
-            <div className="card pb-4" style={{ width: "32rem" }}>
+            <div className="card mb-4" style={{ width: "32rem" }}>
               <div
                 className="card-img-top"
                 style={{
@@ -41,15 +41,15 @@ function TripsListNew() {
                   16/12/2019&nbsp;&nbsp;&nbsp;
                   <span className="text-muted small">TO:</span> 23/01/2020
                   <div className="card-weather">
+                    <div className="dropdown-divider"></div>
                     <span className="text-muted small">WEATHER:</span>
                     <br />
-                    <div className="dropdown-divider"></div>
-                    🥵 hot, 🥶 cold, ☃ snowy, ☔️ rainy, ☀️ sunny
+                    🥵hot, 🥶cold, ☃snowy, ☔️rainy, ☀️sunny
                   </div>
                 </p>
               </div>
             </div>
-            <div className="card pb-4" style={{ width: "32rem" }}>
+            <div className="card mb-5" style={{ width: "32rem" }}>
               <span className="text-muted small">YOUR TRIP IS IN:</span>
               <h2>8 day</h2>
               <span className="text-muted small">PACKING PROGRESS:</span>
@@ -72,8 +72,8 @@ function TripsListNew() {
             <h2>
               <strong>Here's your packing list</strong>
             </h2>
-            <div className="card pb-4" style={{ width: "32rem" }}>
-              <div className="category">
+            <div className="card mb-4 mt-2" style={{ width: "32rem" }}>
+              <div className="category mb-4">
                 <h4> 💵 Documents and money</h4>
                 <div className="dropdown-divider"></div>
                 <div
@@ -94,7 +94,87 @@ function TripsListNew() {
                     />
                   </a>
                 </div>
+                <div
+                  className="form-check item"
+                  style={{ borderBottom: "1px solid #f1f1f1" }}
+                >
+                  &nbsp;
+                  <label data-cy="item-name">Credit cards</label> &nbsp; &nbsp;
+                  <a
+                    className="delete-button float-right"
+                    data-cy="delete-button"
+                  >
+                    <img
+                      src="remove_icon.svg"
+                      alt="Remove item"
+                      title="Remove item"
+                    />
+                  </a>
+                </div>
+                <div className="form-inline mt-3">
+                  <input
+                    type="text"
+                    placeholder="Add an item"
+                    className="form-control mr-2"
+                    style={{ width: "87%" }}
+                  />
+                  <button className="btn btn-secondary">Add</button>
+                </div>
               </div>
+              <div className="category mb-3">
+                <h4> 👕 Clothing</h4>
+                <div className="dropdown-divider"></div>
+                <div
+                  className="form-check item"
+                  style={{ borderBottom: "1px solid #f1f1f1" }}
+                >
+                  <span></span>
+                  &nbsp;
+                  <label data-cy="item-name">Passport</label> &nbsp; &nbsp;
+                  <a
+                    className="delete-button float-right"
+                    data-cy="delete-button"
+                  >
+                    <img
+                      src="remove_icon.svg"
+                      alt="Remove item"
+                      title="Remove item"
+                    />
+                  </a>
+                </div>
+                <div
+                  className="form-check item"
+                  style={{ borderBottom: "1px solid #f1f1f1" }}
+                >
+                  &nbsp;
+                  <label data-cy="item-name">Credit cards</label> &nbsp; &nbsp;
+                  <a
+                    className="delete-button float-right"
+                    data-cy="delete-button"
+                  >
+                    <img
+                      src="remove_icon.svg"
+                      alt="Remove item"
+                      title="Remove item"
+                    />
+                  </a>
+                </div>
+                <div className="form-inline mt-3">
+                  <input
+                    type="text"
+                    placeholder="Add an item"
+                    className="form-control mr-2"
+                    style={{ width: "87%" }}
+                  />
+                  <button className="btn btn-secondary">Add</button>
+                </div>
+              </div>
+              <button
+                type="button"
+                class="btn btn-warning btn-lg btn-block my-3"
+              >
+                SAVE LIST
+              </button>
             </div>
           </div>
         </div>
