640f7825db7d80822303350ada13123b8be64141
Olly Holovchenko <oleh.holo@gmail.com>
2019-12-04 19:02:44 +0000

    Add navbar icons

--- client/src/components/shared/components/Navbar.js
+++ client/src/components/shared/components/Navbar.js
@@ -15,34 +15,23 @@ function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-light bg-white static-top mb-4 shadow">
         <div className="container">
           <NavLink to="/" exact>
-            <h3 className="navbar-brand">
+            <h2 className="navbar-brand">
               <strong>Travelist</strong>
-            </h3>
+            </h2>
           </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div>
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
                 <NavLink to="/start" exact className="nav-link">
-                  New Trip
+                  <i className="far fa-compass nav-icon"> </i> New Trip
                 </NavLink>
               </li>
 
               {auth.isLoggedIn && (
                 <li className="nav-item">
                   <NavLink to="/trips" exact className="nav-link">
-                    My Trips
+                    <i class="fas fa-suitcase-rolling nav-icon"> </i> My Trips
                   </NavLink>
                 </li>
               )}
@@ -50,13 +39,15 @@ function Navbar() {
               {!auth.isLoggedIn && (
                 <li>
                   <NavLink to="/auth">
-                    <button>LOGIN</button>
+                    <button className="nav-button">LOGIN</button>
                   </NavLink>
                 </li>
               )}
               {auth.isLoggedIn && (
                 <li>
-                  <button onClick={auth.logout}>LOGOUT</button>
+                  <button onClick={auth.logout} className="nav-button">
+                    LOGOUT
+                  </button>
                 </li>
               )}
             </ul>
